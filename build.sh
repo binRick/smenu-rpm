dcea5a5fe5d8c5060b48e6232b58c6bc95fc5ada
root <admin@vpntech.net>
2019-06-21 13:01:39 -0400

    refactor script

--- build.sh
+++ build.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+RELEASE_VERSION=$1
+if [ "$RELEASE_VERSION" == "" ]; then
+  echo "First argument must be release version to compile"; exit 1
+fi
+set -e
+
 mkdir -p ~/rpmbuild/SOURCES
-wget wget https://github.com/p-gen/smenu/archive/v0.9.15.tar.gz -O ~/rpmbuild/SOURCES/v0.9.15.tar.gz
-rpmbuild -bb smenu.spec
+
+wget https://github.com/p-gen/smenu/archive/v${RELEASE_VERSION}.tar.gz -O ~/rpmbuild/SOURCES/v${RELEASE_VERSION}.tar.gz
+
+if [ -f "smenu.spec" ]; then
+  unlink smenu.spec
+fi
+
+cp smenu.spec.template .smenu.spec
+
+sed -i "s/__RELEASE_VERSION__/${RELEASE_VERSION}/g" .smenu.spec
+
+
+rpmbuild -bb .smenu.spec
